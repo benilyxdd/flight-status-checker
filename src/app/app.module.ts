634a2798005b3bfb23b771fa26b3c63f3b301caa
benily <leekwokhinben@gmail.com>
2021-08-24 01:28:26 +0800

    add material package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
@@ -7,7 +8,7 @@ import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
 	declarations: [AppComponent, SearchBarComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
