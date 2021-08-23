a4e5ecc6fa5d9d4c4ffdc6bdc5d626a0f16a8cdc
benily <leekwokhinben@gmail.com>
2021-08-24 01:26:12 +0800

    add search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, SearchBarComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
