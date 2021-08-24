f9c1879b90d10ea3d87d09aa4a2fd1f8ee23f285
benily <leekwokhinben@gmail.com>
2021-08-24 18:09:18 +0800

    edit test file

--- src/app/fetch-flight-data/fetch-flight-data.service.spec.ts
+++ src/app/fetch-flight-data/fetch-flight-data.service.spec.ts
@@ -1,9 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FetchFlightDataService } from './fetch-flight-data.service';
+import {
+	HttpClientTestingModule,
+	HttpTestingController,
+} from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 describe('FetchFlightDataService', () => {
 	let service: FetchFlightDataService;
+	let httpClient: HttpClient;
+	let httpTestingController: HttpTestingController;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [HttpClientTestingModule],
+		});
+
+		// Inject the http service and test controller for each test
+		httpClient = TestBed.inject(HttpClient);
+		httpTestingController = TestBed.inject(HttpTestingController);
+	});
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({});
