52620db1b7f63888f4844544fa9a1b9afa09c695
benily <leekwokhinben@gmail.com>
2021-08-24 15:38:16 +0800

    use the search result interface init an empty result

--- src/app/search-result-screen/search-result-screen.component.ts
+++ src/app/search-result-screen/search-result-screen.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { FetchFlightDataService } from '../fetch-flight-data/fetch-flight-data.service';
+import { SearchResult } from './searchResult';
 
 @Component({
 	selector: 'app-search-result-screen',
@@ -8,7 +9,7 @@ import { FetchFlightDataService } from '../fetch-flight-data/fetch-flight-data.s
 	styleUrls: ['./search-result-screen.component.css'],
 })
 export class SearchResultScreenComponent implements OnInit {
-	searchData: object = {};
+	searchData: SearchResult = {} as SearchResult;
 	constructor(private fetchFlightDataService: FetchFlightDataService) {}
 
 	ngOnInit(): void {
