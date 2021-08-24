c64136b6b1a168175dc67c7b97e376ed3cfc6b47
benily <leekwokhinben@gmail.com>
2021-08-25 02:28:29 +0800

    prepare data for api call

--- src/app/fetch-flight-data/fetch-flight-data.service.ts
+++ src/app/fetch-flight-data/fetch-flight-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { auth } from './response';
 
@@ -11,6 +11,30 @@ export class FetchFlightDataService {
 	constructor(private http: HttpClient) {}
 
 	async fetchFlightData(IATA: string): Promise<any> {
+		// prepare data to fetch
+		const carrierCodeArray = IATA.match(/[A-Z]+/g);
+		const carrierCode = carrierCodeArray
+			? carrierCodeArray[0]
+			: 'IMPOSSIBLE';
+		const flightNumberArray = IATA.match(/[0-9]+/g);
+		const flightNumber = flightNumberArray
+			? flightNumberArray[0]
+			: 'IMPOSSIBLE';
+		const currentDate = new Date().toISOString().slice(0, 10);
+		const currentYear = currentDate.slice(0, 4);
+		const currentMonth = currentDate.slice(5, 7);
+		const currentDay = currentDate.slice(8, 10);
+		console.log(currentYear, currentMonth, currentDay);
+		const dataUrl = `https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/${carrierCode}/${flightNumber}/dep/${currentYear}/${currentMonth}/${currentDay}?appId=59740609&appKey=577eb50e53d9ce436a21087f9ff5a6f7&extendedOptions=useInlinedReferences`;
+		const dataHeader = {
+			headers: new HttpHeaders({
+				Accept: 'application/json',
+			}),
+		};
+
+		// const res = await this.http.get(dataUrl, dataHeader).toPromise();
+		// console.log(res);
+
 		// this.data = res;
 	}
 }
