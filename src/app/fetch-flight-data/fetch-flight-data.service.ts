31e3488bb0c6a7c98acbe755fc10d9466a2404d1
benily <leekwokhinben@gmail.com>
2021-08-24 14:47:01 +0800

    implement fetch flight data service

--- src/app/fetch-flight-data/fetch-flight-data.service.ts
+++ src/app/fetch-flight-data/fetch-flight-data.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { api_key } from '../../environments/env';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class FetchFlightDataService {
-	constructor() {}
+	data: any = null;
+	constructor(private http: HttpClient) {}
+
+	fetchFlightData(IATA: string): any {
+		const url = `http://api.aviationstack.com/v1/flights?access_key=${api_key}&flight_iata=${IATA}`;
+		const res = this.http.get(url);
+		this.data = res;
+		return res;
+	}
 }
