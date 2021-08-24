0d9eeee1fa2feea55a9e660b05323bf7a469f64c
benily <leekwokhinben@gmail.com>
2021-08-24 15:16:21 +0800

    create basic thing for search result screen

--- src/app/search-result-screen/search-result-screen.component.ts
+++ src/app/search-result-screen/search-result-screen.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FetchFlightDataService } from '../fetch-flight-data/fetch-flight-data.service';
+
 @Component({
 	selector: 'app-search-result-screen',
 	templateUrl: './search-result-screen.component.html',
 	styleUrls: ['./search-result-screen.component.css'],
 })
 export class SearchResultScreenComponent implements OnInit {
-	constructor() {}
+	searchData: object = {};
+	constructor(private fetchFlightDataService: FetchFlightDataService) {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.searchData = this.fetchFlightDataService.data;
+	}
 }
