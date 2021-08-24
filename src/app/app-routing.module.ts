62963cbc18a6f1d2753091125e04e9b533a1f1e7
benily <leekwokhinben@gmail.com>
2021-08-24 13:34:39 +0800

    add default and search path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SearchBarComponent } from './search-bar/search-bar.component';
+
+const routes: Routes = [
+	{ path: '', redirectTo: '/search', pathMatch: 'full' },
+	{ path: 'search', component: SearchBarComponent },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
