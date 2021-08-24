3fce6e1191462c4198e00ccee60bce624e44d4e5
benily <leekwokhinben@gmail.com>
2021-08-24 23:43:40 +0800

    add auth response interface

---
+++ src/app/fetch-flight-data/response.ts
@@ -0,0 +1,11 @@
+export interface auth {
+	type: String;
+	username: String;
+	application_name: String;
+	client_id: String;
+	token_type: String;
+	access_token: String;
+	expires_in: Number;
+	state: String;
+	scope: String;
+}
