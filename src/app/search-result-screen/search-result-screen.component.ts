1db88a609052c8945d689738066c682e4093b275
benily <leekwokhinben@gmail.com>
2021-08-26 19:22:03 +0800

    update time string to a nice one

--- src/app/search-result-screen/search-result-screen.component.ts
+++ src/app/search-result-screen/search-result-screen.component.ts
@@ -52,6 +52,192 @@ export class SearchResultScreenComponent implements OnInit {
 
 			// status
 			this.status = FlightStatusDescription[this.searchData.status];
+
+			// time
+			this.searchData.departureDate.dateLocal = this.parseTime(
+				this.searchData.departureDate.dateLocal
+			);
+			this.searchData.arrivalDate.dateLocal = this.parseTime(
+				this.searchData.arrivalDate.dateLocal
+			);
+
+			if (
+				this.searchData.operationalTimes.publishedDeparture !==
+				undefined
+			) {
+				this.searchData.operationalTimes.publishedDeparture.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes.publishedDeparture
+							.dateLocal
+					);
+			}
+
+			if (
+				this.searchData.operationalTimes.publishedArrival !== undefined
+			) {
+				this.searchData.operationalTimes.publishedArrival.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes.publishedArrival
+							.dateLocal
+					);
+			}
+
+			if (
+				this.searchData.operationalTimes.scheduledGateDeparture !==
+				undefined
+			) {
+				this.searchData.operationalTimes.scheduledGateDeparture.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes.scheduledGateDeparture
+							.dateLocal
+					);
+			}
+
+			if (
+				this.searchData.operationalTimes.scheduledRunwayDeparture !==
+				undefined
+			) {
+				this.searchData.operationalTimes.scheduledRunwayDeparture.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes
+							.scheduledRunwayDeparture.dateLocal
+					);
+			}
+
+			if (
+				this.searchData.operationalTimes.estimatedGateDeparture !==
+				undefined
+			) {
+				this.searchData.operationalTimes.estimatedGateDeparture.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes.estimatedGateDeparture
+							.dateLocal
+					);
+			}
+
+			if (
+				this.searchData.operationalTimes.actualGateDeparture !==
+				undefined
+			) {
+				this.searchData.operationalTimes.actualGateDeparture.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes.actualGateDeparture
+							.dateLocal
+					);
+			}
+
+			if (
+				this.searchData.operationalTimes.flightPlanPlannedDeparture !==
+				undefined
+			) {
+				this.searchData.operationalTimes.flightPlanPlannedDeparture.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes
+							.flightPlanPlannedDeparture.dateLocal
+					);
+			}
+
+			if (
+				this.searchData.operationalTimes.estimatedRunwayDeparture !==
+				undefined
+			) {
+				this.searchData.operationalTimes.estimatedRunwayDeparture.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes
+							.estimatedRunwayDeparture.dateLocal
+					);
+			}
+
+			if (
+				this.searchData.operationalTimes.actualRunwayDeparture !==
+				undefined
+			) {
+				this.searchData.operationalTimes.actualRunwayDeparture.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes.actualRunwayDeparture
+							.dateLocal
+					);
+			}
+
+			if (
+				this.searchData.operationalTimes.scheduledRunwayArrival !==
+				undefined
+			) {
+				this.searchData.operationalTimes.scheduledRunwayArrival.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes.scheduledRunwayArrival
+							.dateLocal
+					);
+			}
+
+			if (
+				this.searchData.operationalTimes.scheduledGateArrival !==
+				undefined
+			) {
+				this.searchData.operationalTimes.scheduledGateArrival.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes.scheduledGateArrival
+							.dateLocal
+					);
+			}
+
+			if (
+				this.searchData.operationalTimes.estimatedGateArrival !==
+				undefined
+			) {
+				this.searchData.operationalTimes.estimatedGateArrival.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes.estimatedGateArrival
+							.dateLocal
+					);
+			}
+
+			if (
+				this.searchData.operationalTimes.actualGateArrival !== undefined
+			) {
+				this.searchData.operationalTimes.actualGateArrival.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes.actualGateArrival
+							.dateLocal
+					);
+			}
+
+			if (
+				this.searchData.operationalTimes.flightPlanPlannedArrival !==
+				undefined
+			) {
+				this.searchData.operationalTimes.flightPlanPlannedArrival.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes
+							.flightPlanPlannedArrival.dateLocal
+					);
+			}
+
+			if (
+				this.searchData.operationalTimes.estimatedRunwayArrival !==
+				undefined
+			) {
+				this.searchData.operationalTimes.estimatedRunwayArrival.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes.estimatedRunwayArrival
+							.dateLocal
+					);
+			}
+
+			if (
+				this.searchData.operationalTimes.actualRunwayArrival !==
+				undefined
+			) {
+				this.searchData.operationalTimes.actualRunwayArrival.dateLocal =
+					this.parseTime(
+						this.searchData.operationalTimes.actualRunwayArrival
+							.dateLocal
+					);
+			}
 		}
+	}
+
+	private parseTime(time: string): string {
+		return new Date(time).toLocaleString();
 	}
 }
