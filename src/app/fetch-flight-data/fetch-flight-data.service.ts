371f56379a4f3fd7a1f2edbce804780e16dd4b67
benily <leekwokhinben@gmail.com>
2021-08-26 17:08:37 +0800

    use thingproxy to solve cros problem

--- src/app/fetch-flight-data/fetch-flight-data.service.ts
+++ src/app/fetch-flight-data/fetch-flight-data.service.ts
@@ -17,7 +17,7 @@ export class FetchFlightDataService {
 		const currentYear = currentDate.slice(0, 4);
 		const currentMonth = currentDate.slice(5, 7);
 		const currentDay = currentDate.slice(8, 10);
-		const dataUrl = `https://cors-anywhere.herokuapp.com/https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/${carrierCode}/${flightNumber}/dep/${currentYear}/${currentMonth}/${currentDay}?appId=59740609&appKey=${app_key}&extendedOptions=useInlinedReferences`;
+		const dataUrl = `https://thingproxy.freeboard.io/fetch/https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/${carrierCode}/${flightNumber}/dep/${currentYear}/${currentMonth}/${currentDay}?appId=59740609&appKey=${app_key}&extendedOptions=useInlinedReferences`;
 
 		const res = await this.http.get(dataUrl).toPromise();
 		this.data = res;
